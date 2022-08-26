2679e92a947bba9a663e9d5cf1165645ba9d9eb7
Nishant Kumar <nkgunjan26102003@gmail.com>
2022-08-26 18:15:46 +0530

    added day-2 complete task

---
+++ index.js
@@ -0,0 +1,31 @@
+  window.addEventListener("load",()=>{
+    SlideShow()
+  })
+   let address;
+   img_url=[
+       'https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2FDesktop_Banner_Shine.png&w=1920&q=75',
+       'https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2FShine_2nd_pos_Desktop.png&w=1920&q=75',
+       'https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2FReliance_Desktop.png&w=1920&q=75',
+       'https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2FInfosys.png&w=1920&q=75',
+       'https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2Fmint_d_banner.png&w=1920&q=75',
+       'https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2Fwinter_d_home.png&w=1920&q=75', 
+   ]
+  
+let i =0;
+let img = document.createElement('img');
+ let SlideShow =()=>{
+   let slide_cont = document.getElementById('container');
+ 
+
+   address = setInterval(()=>{
+
+       slide_cont.innerHTML=null;
+       if(i==img_url.length ){
+           i=0;
+       }
+      
+       img.src = img_url[i];
+       slide_cont.append(img);
+       i++;
+   },1600)
+ }
